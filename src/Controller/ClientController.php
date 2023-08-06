29038e39182734c1537ef15f183d9fc36b9e952e
camila-falaschi <falaschi.camila@yahoo.com.br>
2023-08-07 00:51:16 +0200

    feat: adding style to clients list page

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -17,14 +17,6 @@ class ClientController extends Controller
     {
         $clients_data = json_decode(file_get_contents(__DIR__ . '/../../data/clienti.json'), true);
         
-        require_once __DIR__ . '/../../helpers/functions.php';
-
-        $twig = $this->ci->get('templating');
-
-        $twig->addFunction(new \Twig\TwigFunction('filter_by_status', 'filter_by_status'));
-        $twig->addFunction(new \Twig\TwigFunction('calculate_total_price', 'calculate_total_price'));
-        $twig->addFunction(new \Twig\TwigFunction('filtered_data', 'filtered_data'));
-
         return $this->render($response, 'clients/clients.twig', [
             'clients_data' => $clients_data,
         ]);
